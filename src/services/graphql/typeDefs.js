973c3d9829985e33aa60eea1662bddfa046ec0bf
Fabian Beliza <fabian.beliza@iem.thm.de>
2016-12-14 16:28:11 +0100

    Add documentation to typeDefs

--- src/services/graphql/typeDefs.js
+++ src/services/graphql/typeDefs.js
@@ -1,39 +1,49 @@
 // @flow
 export default `
 
+# A base64 encoded \`Image\`
 scalar Image
 
+# A list of BBQ methods
 enum BBQMethod {
   DIRECT
   INDIRECT
 }
 
+# A list of BBQ intensities
 enum BBQIntensity{
   STRONG
   MEDIUM
   LOW
 }
 
+# A User/Creator/Publisher
 type User {
   _id: String! # the ! means that every author object _must_ have an id
   firstName: String
   lastName: String
   username: String!
+  # The collection of recipes the User has published
   recipes: [Recipe]
+  # The collection of cookbooks the User has published
   cookbooks: [Cookbook]
 }
 
+# A Recipe
 type Recipe {
   _id: String!
   title: String!
   description: String
+  # A collection of tags to filter recipes for
   tags: [String]
+  # A collection of Tasks to follow
   sequence: [Task]
   createdAt: String
   author: User
   thumbnail(width: Int, height: Int, greyscale: Boolean): Image
 }
 
+# The task will be part of a sequence the user has to follow to create a dish
 type Task {
   title: String!
   duration: Int
@@ -41,28 +51,34 @@ type Task {
   method: BBQMethod
 }
 
+# A cookbook that contains a collection of recipes
 type Cookbook {
   title: String!
   description: String
+  # A collection of tags to filter cookbooks for
   tags: [String]
   recipes: [Recipe]
   createdAt: String
   author: User
 }
 
+# The payload which is needed to Authenticate
 type AuthPayload {
+  # JSON Web Token
   token: String # JSON Web Token
   data: User
 }
 
-input recipeInput {
+# Input for Recipe
+input RecipeInput {
   title: String!
   description: String!
   tags: [String]
-  sequence: [taskInput]
+  sequence: [TaskInput]
 }
 
-input taskInput {
+# Input for Tasks
+input TaskInput {
   title: String!
   duration: Int
   intensity: BBQIntensity
@@ -93,7 +109,7 @@ type RootMutation {
   ): AuthPayload
 
   createRecipe (
-    recipe: recipeInput
+    recipe: RecipeInput
     imageUrl: String
   ): Recipe
 
